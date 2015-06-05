afb021187280c87818e5a643b4618d05e0de6763
Alex Miccio <miccio.alex@gmail.com>
2015-06-05 23:09:49 +0200

    Improved code and fields name are now more intuitive

--- src/client/Client.java
+++ src/client/Client.java
@@ -21,24 +21,23 @@ public class Client
     
     public void connect() throws IOException
     {
-        System.out.println("Attempting to connect to " + this.server + ":" + this.port);
+        System.out.println("Attempting to connect to server " + this.server + ":" + this.port + "...");
         this.client = new Socket(this.server, this.port);
-        System.out.println("Connection established");
+        System.out.println("Connection to server " + this.server + ":" + this.port + " established");
     }
     
-    public void askForChoice() throws IOException
+    public void askForTimeToServer() throws IOException
     {
-        System.out.println("Asking for choice...");
-        OutputStream outToServer = this.client.getOutputStream();
-        DataOutputStream out = new DataOutputStream(outToServer);
-        out.writeUTF("Choice?");
+        System.out.println("Asking for time to " + this.server + this.port + "...");
+        DataOutputStream outToServer = new DataOutputStream(this.client.getOutputStream());
+        outToServer.writeUTF("Time?");
     }
     
-    public void readChoice() throws IOException
+    public void readTimeFromServer() throws IOException
     {
         InputStream inFromServer = client.getInputStream();
         DataInputStream in = new DataInputStream(inFromServer);
-        System.out.println("Choice: " + in.readUTF());
+        System.out.println("Time: " + in.readUTF());
     }
     
     public static void main(String[] args)
@@ -47,28 +46,8 @@ public class Client
         try
         {
             client.connect();
-            
-            client.askForChoice();
-            client.readChoice();
-        }
-        catch(UnknownHostException exception)
-        {
-            System.err.println("Host unknown");
-            System.err.println("Cannot establish connection");
-        }
-        catch(IOException exception)
-        {
-            System.err.println("Cannot establish connection");
-            System.err.println("Server may not be up");
-        }
-        
-        client = new Client("localhost", 9999);
-        try
-        {
-            client.connect();
-            
-            client.askForChoice();
-            client.readChoice();
+            client.askForTimeToServer();
+            client.readTimeFromServer();
         }
         catch(UnknownHostException exception)
         {
